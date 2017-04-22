94aae5ad6faebc08b47109334019272dbdcef946
gSilas <d.micheel@outlook.com>
2017-04-23 01:01:12 +0200

    rain frequency tuning

--- host/scripts/rain.py
+++ host/scripts/rain.py
@@ -9,7 +9,8 @@ class rain(CustomScript):
                  set_frame_rate, get_connected_clients):
         super().__init__(canvas, send_object, send_object_to_all, start_script, restart_self, set_frame_period,
                          set_frame_rate, get_connected_clients)
-        self.set_frame_period(1)
+        self.set_frame_period(0.2)
+        self.frame = 0
 
     def update(self, canvas):
         # count the number of neighbours
@@ -22,8 +23,11 @@ class rain(CustomScript):
                     canvas.draw_pixel(x, y-1, c)
 
     def draw(self, canvas):
-        for i in range(0, 1):
-            canvas.draw_pixel(randint(0,canvas.width-1), 0, Color(red = randint(0,255)/255, green = randint(0,255)/255, blue = randint(0,255)/255))
+        self.frame+=1
+        if(self.frame%3 == 0):
+            frame = 0
+            for i in range(0, 2):
+                canvas.draw_pixel(randint(0,canvas.width-1), 0, Color(red = randint(0,255)/255, green = randint(0,255)/255, blue = randint(0,255)/255))
 
 
         #print(repr(canvas))
