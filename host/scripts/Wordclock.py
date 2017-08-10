be902c4950a775f38844e0780c505fe39851a5a1
enra64 <sujomear@arcor.de>
2017-08-10 13:11:28 +0200

    working on wordclock custom script

--- host/scripts/Wordclock.py
+++ host/scripts/Wordclock.py
@@ -1,5 +1,6 @@
-from typing import List
 import datetime
+from typing import List
+import logging
 
 from helpers.Color import Color
 
@@ -7,11 +8,27 @@ from CustomScript import CustomScript
 from Canvas import Canvas, Rect
 
 
-class Wordclock:
-    def __init__(self):
+def round_to_five(integer: int):
+    return int(5 * round(float(integer) / 5))
+
+
+class Wordclock(CustomScript):
+    def __init__(self, canvas, send_object, send_object_to_all, start_script, restart_self, set_frame_period,
+                 set_frame_rate, get_connected_clients):
+        super().__init__(canvas, send_object, send_object_to_all, start_script, restart_self, set_frame_period,
+                         set_frame_rate, get_connected_clients)
+        # request updates to happen in 5-second-intervals
+        set_frame_period(10)
+
+        self.__create_word_rectangles()
+
+        self.logger = logging.getLogger("script:wordclock")
+
+    def __create_word_rectangles(self):
         self.others = {
             "to": Rect(0, 3, 2, 1),
-            "past": Rect(2, 3, 4, 1)
+            "past": Rect(2, 3, 4, 1),
+            "minutes": Rect(2, 3, 4, 1)
         }
 
         self.minutes = {
@@ -38,42 +55,32 @@ class Wordclock:
             11: Rect(0, 6, 6, 1)
         }
 
-        pass
+    def __get_rectangles(self, now_time) -> List[Rect]:
+        out_rectangles = []
 
-    @staticmethod
-    def __round_to_five(integer:int):
-        return int(5 * round(float(integer) / 5))
-
-    def get_rectangles(self, now_time) -> List[Rect]:
-        rects = []
-
-        rounded_time = self.__round_to_five(now_time.minute)
+        rounded_minutes = round_to_five(now_time.minute)
 
         if now_time.minute < 35:
-            rects.append(self.others["past"])
-            rects.append(self.minutes[rounded_time])
-
+            out_rectangles.append(self.minutes[rounded_minutes])
+            if not (rounded_minutes == 15 or rounded_minutes == 30):
+                out_rectangles.append(self.others["minutes"])
+            out_rectangles.append(self.others["past"])
+            out_rectangles.append(self.hour_rects[now_time.hour % 12])
         else:
-            rects.append(self.others["to"])
-            rects.append(self.minutes[60 - rounded_time])
-            rects.append(self.hour_rects[(now_time.hour + 1)% 12])
+            out_rectangles.append(self.minutes[60 - rounded_minutes])
+            if rounded_minutes != 15:
+                out_rectangles.append(self.others["minutes"])
+            out_rectangles.append(self.others["to"])
+            out_rectangles.append(self.hour_rects[(now_time.hour + 1) % 12])
 
-        return rects
-
-
-class Flashlight(CustomScript):
-    def __init__(self, canvas, send_object, send_object_to_all, start_script, restart_self, set_frame_period,
-                 set_frame_rate, get_connected_clients):
-        super().__init__(canvas, send_object, send_object_to_all, start_script, restart_self, set_frame_period,
-                         set_frame_rate, get_connected_clients)
-        self.enable = True
+        return out_rectangles
 
     def draw(self, canvas: Canvas):
-        if self.enable:
-            canvas.clear(Color(255, 255, 255))
-        else:
-            canvas.clear()
+        word_rectangles = self.__get_rectangles(datetime.datetime.now())
+        print(len(word_rectangles))
+        canvas.clear()
+        for rectangle in word_rectangles:
+            canvas.draw_rectangle(rectangle, Color(0, 255, 0))
 
     def on_data(self, json, source_id):
-        if 'enable' in json:
-            self.enable = json['enable']
+        pass
