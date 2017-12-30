7c6ac8ba06d2946a71e3b2f675a41e99a41b2186
enra64 <sujomear@arcor.de>
2017-12-30 14:18:20 +0100

    added class for tracking devices with means other than the zmq id to be able to survive disconnects

---
+++ client-android/MatLedControl/app/src/main/java/de/oerntec/matledcontrol/networking/Installation.java
@@ -0,0 +1,47 @@
+package de.oerntec.matledcontrol.networking;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.UUID;
+
+/**
+ * Taken from https://android-developers.googleblog.com/2011/03/identifying-app-installations.html.
+ * This class allows to uniquely identify each installation of the app.
+ */
+public class Installation {
+    private static String sID = null;
+    private static final String INSTALLATION = "INSTALLATION";
+
+    public synchronized static String id(Context context) {
+        if (sID == null) {
+            File installation = new File(context.getFilesDir(), INSTALLATION);
+            try {
+                if (!installation.exists())
+                    writeInstallationFile(installation);
+                sID = readInstallationFile(installation);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sID;
+    }
+
+    private static String readInstallationFile(File installation) throws IOException {
+        RandomAccessFile f = new RandomAccessFile(installation, "r");
+        byte[] bytes = new byte[(int) f.length()];
+        f.readFully(bytes);
+        f.close();
+        return new String(bytes);
+    }
+
+    private static void writeInstallationFile(File installation) throws IOException {
+        FileOutputStream out = new FileOutputStream(installation);
+        String id = UUID.randomUUID().toString();
+        out.write(id.getBytes());
+        out.close();
+    }
+}
